3857f6b96a5b270271074c28966744d50982236e
R. Steiner <r.steiner@uconn.edu>
2017-02-16 00:20:41 -0500

    Comment 'makefile.'

--- attention/make-beamer.sh
+++ attention/make-beamer.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+# In a terminal, run `bash make-beamer.sh filename-without-extension`
+# So for this, I would run `bash make-beamer.sh tg80slides`
 pandoc -t beamer -o $1.pdf $1.md -V theme:Rochester -V colortheme:beetle
