6e31c84efcbd64679f0dc19e7c10e1cf1490af0b
R. Steiner <r.steiner@uconn.edu>
2017-03-08 16:37:41 -0500

    Comment everything out, now trying to work with Python 3.6.

--- language/grammar.py
+++ language/grammar.py
@@ -15,91 +15,91 @@ with open("syngrams.json", "r") as f:
 # Parts of Speech #
 ###################
 
-mini_conj = ["and", "or"]
-conjunctions = mini_conj + ["but", "because", "though", "yet", "while",
-                            "whereas"]
-determiners = ["the", "a(n)", "my", "her", "his", "their", "this", "that",
-               "every", "everyone's", "{}'s".format(ran(proper_noun))]
-nouns = ngrams["NN"]
-proper_nouns = ngrams["NNP"]
-adjectives = ngrams["JJ"]
-adverbs = ngrams["RB"]
-prepositions = ["of", "on", "in", "under", "for"]
-helping_verbs = ["is/are"]
-
-#################
-# Phrases, etc. #
-#################
-
-
-def prep_phrase():
-    return "{} {}".format(prep(), types_np())
-
-
-def verb_phrase():
-    sub_vp = ["{}".format(intransitive_verb()),
-              "{} {}".format(transitive_verb(), noun_phrase())]
-
-    return ran(sub_vp + ["{} {}".format(helping_verb(), adjective()),
-                         "{} {}".format(helping_verb(), noun_phrase()),
-                         "{} {}".format(ran(sub_vp), prep_phrase()),
-                         "{} {}".format(intransitive_verb(), adverb()),
-                         "{} {} {}".format(adverb(), transitive_verb(),
-                                           noun_phrase())
-                         ])
-
-
-def noun_phrase():
-    return ran([types_np(),
-                "{} {} {}".format(types_np(), conj(), types_np()),
-                "{} {}".format(types_np(), prep_phrase())
-                ])
-
-
-def clause():
-    return "{} {}".format(noun_phrase(), verb_phrase())
-
-
-def sentence():
-    out = ran(["{}.".format(clause()),
-               "{}, {} {}.".format(clause(), coord_conj(), clause())])
-    # out = out.capitalize()
-    print(out)
-    # return out
-
-
-# def main():
-#     sentence()
-
-
-####################
-# Helper Functions #
-####################
-
-
-def ran(spl):
-    return rd.choice(spl)
-
-noun = (rd.choices(ngrams["NN"].keys(), weights=ngrams["NN"].values()) while True)
-adjective = (rd.choices(ngrams["JJ"].keys(), weights=ngrams["JJ"].values()) while True)
+# mini_conj = ["and", "or"]
+# conjunctions = mini_conj + ["but", "because", "though", "yet", "while",
+#                             "whereas"]
+# determiners = ["the", "a(n)", "my", "her", "his", "their", "this", "that",
+#                "every", "everyone's", "{}'s".format(ran(proper_noun))]
+# nouns = ngrams["NN"]
+# proper_nouns = ngrams["NNP"]
+# adjectives = ngrams["JJ"]
+# adverbs = ngrams["RB"]
+# prepositions = ["of", "on", "in", "under", "for"]
+# helping_verbs = ["is/are"]
+#
+# #################
+# # Phrases, etc. #
+# #################
+#
+#
+# def prep_phrase():
+#     return "{} {}".format(prep(), types_np())
+#
+#
+# def verb_phrase():
+#     sub_vp = ["{}".format(intransitive_verb()),
+#               "{} {}".format(transitive_verb(), noun_phrase())]
+#
+#     return ran(sub_vp + ["{} {}".format(helping_verb(), adjective()),
+#                          "{} {}".format(helping_verb(), noun_phrase()),
+#                          "{} {}".format(ran(sub_vp), prep_phrase()),
+#                          "{} {}".format(intransitive_verb(), adverb()),
+#                          "{} {} {}".format(adverb(), transitive_verb(),
+#                                            noun_phrase())
+#                          ])
+#
+#
+# def noun_phrase():
+#     return ran([types_np(),
+#                 "{} {} {}".format(types_np(), conj(), types_np()),
+#                 "{} {}".format(types_np(), prep_phrase())
+#                 ])
+#
+#
+# def clause():
+#     return "{} {}".format(noun_phrase(), verb_phrase())
+#
+#
+# def sentence():
+#     out = ran(["{}.".format(clause()),
+#                "{}, {} {}.".format(clause(), coord_conj(), clause())])
+#     # out = out.capitalize()
+#     print(out)
+#     # return out
+#
+#
+# # def main():
+# #     sentence()
+#
+#
+# ####################
+# # Helper Functions #
+# ####################
+#
+#
+# def ran(spl):
+#     return rd.choice(spl)
+#
+noun = (rd.choices(ngrams["NN"].keys(), weights=ngrams["NN"].values()))
+adjective = (rd.choices(ngrams["JJ"].keys(), weights=ngrams["JJ"].values()))
 
 nominal = (rd.choice([next(noun), "{} {}".format(next(adjective), next(noun))]))
 
-def nominal():
-    return ran([noun(), "{} {}".format(adjective(), noun())])
-
-
-def combo_np():
-    return "{} {}".format(det(), nominal())
-
-
-def types_np():
-    return ran([proper_noun(), combo_np()])
-
-
-###################
-# Run the program #
-###################
-
-if __name__ == "__main__":
-    sentence()
+# def nominal():
+#     return ran([noun(), "{} {}".format(adjective(), noun())])
+#
+#
+# def combo_np():
+#     return "{} {}".format(det(), nominal())
+#
+#
+# def types_np():
+#     return ran([proper_noun(), combo_np()])
+#
+#
+# ###################
+# # Run the program #
+# ###################
+#
+# if __name__ == "__main__":
+#     sentence()
