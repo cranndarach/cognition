c55cfb782636ce54b91fd11fa712cbf9f60be2df
R. Steiner <r.steiner@uconn.edu>
2017-03-21 16:27:32 -0400

    Use generators for nominals and NPs.

--- language/grammar.py
+++ language/grammar.py
@@ -2,6 +2,7 @@
 
 import random as rd
 import json
+# import sys
 # import numpy as np
 
 ##########################
@@ -11,22 +12,28 @@ import json
 with open("syngrams.json", "r") as f:
     ngrams = json.load(f)
 
+# print(len(ngrams["NN"]))
+# sys.exit(0)
+
+
+def expand_lex(lex):
+    return [word for word, freq in lex.items() for _ in range(int(freq/10000))]
+
+
 ###################
 # Parts of Speech #
 ###################
 
-# mini_conj = ["and", "or"]
-# conjunctions = mini_conj + ["but", "because", "though", "yet", "while",
-#                             "whereas"]
-# determiners = ["the", "a(n)", "my", "her", "his", "their", "this", "that",
-#                "every", "everyone's", "{}'s".format(ran(proper_noun))]
-# nouns = ngrams["NN"]
-# proper_nouns = ngrams["NNP"]
-# adjectives = ngrams["JJ"]
-# adverbs = ngrams["RB"]
-# prepositions = ["of", "on", "in", "under", "for"]
-# helping_verbs = ["is/are"]
-#
+nouns = expand_lex(ngrams["NN"])
+adjectives = expand_lex(ngrams["JJ"])
+conjunctions = expand_lex(ngrams["CC"])
+prepositions = expand_lex(ngrams["IN"])
+proper_nouns = expand_lex(ngrams["NNP"])
+determiners = expand_lex(ngrams["DT"])
+personal_pronouns = expand_lex(ngrams["PRP"])
+adverbs = expand_lex(ngrams["RB"])
+verbs = expand_lex(ngrams["VBZ"])
+
 # #################
 # # Phrases, etc. #
 # #################
@@ -75,15 +82,57 @@ with open("syngrams.json", "r") as f:
 # ####################
 # # Helper Functions #
 # ####################
-#
-#
+
+
+def word_gen(words):
+    while True:
+        yield rd.choice(words)
+
+
+def phrase_gen(*args):
+    # if "list" in list(map(type, args)):
+    #     choices = []
+    #     for arg in args:
+    #         if type(arg) == "list":
+    string_template = args[0]
+    while True:
+        yield string_template.format(next(gen) for gen in args[1:])
+
+
+def option_gen(*options):
+    while True:
+        yield rd.choice(options)
+
+
+# def formatter(arg):
+#     string_template = arg[0]
+#     return string_template.format([next(gen) for gen in arg[1:]])
+
+
 # def ran(spl):
 #     return rd.choice(spl)
-#
-noun = (rd.choices(ngrams["NN"].keys(), weights=ngrams["NN"].values()))
-adjective = (rd.choices(ngrams["JJ"].keys(), weights=ngrams["JJ"].values()))
 
-nominal = (rd.choice([next(noun), "{} {}".format(next(adjective), next(noun))]))
+
+noun = word_gen(nouns)
+adjective = word_gen(adjectives)
+det = word_gen(determiners)
+
+# nominal = option_gen(next(noun), next(phrase_gen("{} {}", adjective, noun)))
+# noun_phrase = phrase_gen("{} {}", det, nominal)
+
+
+def noun_phrase():
+    while True:
+        yield "{} {}".format(next(det), next(nom))
+np = noun_phrase()
+
+
+def nominal():
+    while True:
+        options = [next(noun),
+                   "{} {}".format(next(adjective), next(noun))]
+        yield rd.choice(options)
+nom = nominal()
 
 # def nominal():
 #     return ran([noun(), "{} {}".format(adjective(), noun())])
