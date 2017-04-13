23da33992470a42435113a7d3b459811f34652b4
R. Steiner <r.steiner@uconn.edu>
2017-04-13 12:26:37 -0400

    Remove comment.

--- fractals/integers.hs
+++ fractals/integers.hs
@@ -2,5 +2,4 @@
 -- integers as a countable infinity.
 
 integerMap :: Int -> Int
--- integerMap n = (-1 * ((n `mod` 2) + 1)) * (n - (n `mod` 2)) `div` 2
 integerMap n = (n - n `mod` 2) * (-1)^(n `mod` 2) `div` 2
